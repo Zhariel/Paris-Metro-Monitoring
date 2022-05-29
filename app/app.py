1f488a5f218ab539bda7ace741c6e3fbeb220353
Romain Blazevic <romain.blazevic@gmail.com>
2022-05-30 00:49:54 +0200

    p5 integration

--- app/app.py
+++ app/app.py
@@ -1,15 +1,32 @@
+import flask
 from flask import Flask, send_from_directory
 # from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS #comment this on deployment
+from flask_cors import CORS  # comment this on deployment
+from flask import request
 from flask import Flask
+import json
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+JSON_PATH = os.path.join('data', 'stations_list.json')
+
+
+@app.route('/health')
+def health():
+    return "up"
+
 
 @app.route('/stations')
-def serve():
-    return "aaaaa"
+def stations():
+    file = open(JSON_PATH)
+    json_object = (json.load(file))
+    print(type(json_object))
+    # print(json_object)
+
+    return json_object
+
 
 if __name__ == '__main__':
     app.run()
--- app/frontend/scripts/fetch_example.js
+++
@@ -1,7 +0,0 @@
-const url = "http://127.0.0.1:5000/stations";
-
-fetch(url)
-    // .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    });
