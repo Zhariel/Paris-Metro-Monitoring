7370dc1ce78acf91a3abc2eabeedcefef11217ec
Romain Blazevic <romain.blazevic@gmail.com>
2022-06-18 17:09:49 +0200

    formatted client.py

--- api/client.py
+++ api/client.py
@@ -1,73 +1,98 @@
 import json
-#from api import *
+from ntpath import join
+
+# from api import *
+from utils import join_path
 from settings import *
 import split_data as data_utils
+import pandas as pd
 import requests
 import csv
-import pandas as pd
 import io
+
 
 class Client:
     def __init__(self):
         self.headers = HEADERS
-        self.url = URL
-
+        self.url_base = URL
 
     def factor_data(self, transportMode):
-        uurl            = self.url + "coverage/fr-idf/physical_modes/physical_mode:" + transportMode + "/lines"
-        response        = requests.get(uurl, headers=self.headers).json()
-        disruptions     = response["disruptions"]
-        lines           = response["lines"]
-        df_dispuptions  = data_utils.get_disruptions(disruptions)
-        df_lines        = data_utils.get_lines(lines)
+        uurl = join_path(
+            self.url_base,
+            "coverage",
+            "fr-idf",
+            "physical_modes",
+            f"physical_mode:{transportMode}",
+            "lines",
+        )
+        response = requests.get(uurl, headers=self.headers).json()
+        disruptions = response["disruptions"]
+        lines = response["lines"]
+        df_dispuptions = data_utils.get_disruptions(disruptions)
+        df_lines = data_utils.get_lines(lines)
 
         return df_dispuptions, df_lines
 
     def df_to_csv(self, df_dispuptions, df_lines, disruptions_file, lines_file):
-        path                = "../data/navitia/"
-        disruptions_path    = path + disruptions_file
-        lines_path          = path + lines_file
-        df_dispuptions.to_csv(disruptions_path,index=False, sep=';')
-        df_lines.to_csv(lines_path,index=False, sep=';')
+        path = join_path("..", "data", "navitia")
+        disruptions_path = join_path(path, disruptions_file)
+        lines_path = join_path(path, lines_file)
+        df_dispuptions.to_csv(disruptions_path, index=False, sep=";")
+        df_lines.to_csv(lines_path, index=False, sep=";")
 
     def get_all_Metro_lines_json(self):
         df_dispuptions, df_lines = self.factor_data("Metro")
-        self.df_to_csv(df_dispuptions, df_lines,"metros/disruptions_metros.csv","metros/lines_metros.csv")
+        self.df_to_csv(
+            df_dispuptions,
+            df_lines,
+            join_path("metros", "disruptions_metros.csv"),
+            join_path("metros", "lines_metros.csv"),
+        )
 
     def get_all_Bus_lines_json(self):
         df_dispuptions, df_lines = self.factor_data("Bus")
-        self.df_to_csv(df_dispuptions, df_lines,"bus/disruptions_bus.csv","bus/lines_bus.csv")
+        self.df_to_csv(
+            df_dispuptions,
+            df_lines,
+            join_path("bus", "disruptions_bus.csv"),
+            join_path("bus", "lines_bus.csv"),
+        )
 
     def get_all_Tramway_lines_json(self):
         df_dispuptions, df_lines = self.factor_data("Tramway")
-        self.df_to_csv(df_dispuptions, df_lines,"tramways/disruptions_tram.csv","tramways/lines_tramways.csv")
-
+        self.df_to_csv(
+            df_dispuptions,
+            df_lines,
+            join_path("tramways", "disruptions_tram.csv"),
+            join_path("tramways", "lines_tramways.csv"),
+        )
 
     def get_all_LocalTrains_lines_json(self):
         df_dispuptions, df_lines = self.factor_data("LocalTrain")
-        self.df_to_csv(df_dispuptions, df_lines,"localtrains/disruptions_localtrains.csv","localtrains/lines_localtrains.csv")
-
+        self.df_to_csv(
+            df_dispuptions,
+            df_lines,
+            join_path("localtrains", "disruptions_localtrains.csv"),
+            join_path("localtrains", "lines_localtrains.csv"),
+        )
 
     def get_all_RapidTransit_lines_json(self):
         df_dispuptions, df_lines = self.factor_data("RapidTransit")
-        self.df_to_csv(df_dispuptions, df_lines,"rapidtransits/disruptions_rapidtransit.csv","rapidtransits/lines_rapidtransits.csv")
-
+        self.df_to_csv(
+            df_dispuptions,
+            df_lines,
+            join_path("rapidtransits", "disruptions_rapidtransit.csv"),
+            join_path("rapidtransits", "lines_rapidtransits.csv"),
+        )
 
     # Get all close points
-    def get_all_pois_json(self, suffix = "pois/"):
-        uurl = self.url + suffix
+    def get_all_pois_json(self, suffix="pois/"):
+        uurl = self.url_base + suffix
         response = requests.get(uurl, headers=self.headers)
 
-    def get_all_poi_types_json(self, suffix = "poi_types/"):
-        uurl = self.url + suffix
+    def get_all_poi_types_json(self, suffix="poi_types/"):
+        uurl = self.url_base + suffix
         response = requests.get(uurl, headers=self.headers)
-
-
-
-
-
-
-
 
 
 client = Client()
---
+++ api/utils.py
@@ -0,0 +1,5 @@
+
+import os
+
+def join_path(*args):
+    return os.path.join(*args)
